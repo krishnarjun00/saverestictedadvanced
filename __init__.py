a9c8314794a38b6b74713eea7441eb0eeb7fd97f
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-19 14:47:37 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,12 +15,12 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None)
+API_ID = 26765615
+API_HASH = "dc9335059a80b3c8753d26e36d9e28f4"
+BOT_TOKEN = "6992559223:AAEMce2gy8ALQPi7pLe8I6PLfjbuOTWBlHc"
+SESSION = "BQGYaS8ASNnD0e3uXTGHnO-dhEbCVIxOiDLhQjtg1CB_PD9nw3RSa2nL1P2wlPAsRqc8mKd8pgEDsz1ubVCaPQ_STaNvOQWAAdmx2uUx-FAimoCTDwgvIpWqS9aKoF2tH-ZCEVNbAkbvgEQBMV9ZPtQ0bYdPJ5LRvzSyOUlVGOX1ao_D6Cw_tl2LrxU9WatUlEv6TB1JFEcVIi6RrAe_IpjmF5RMSJdH8TPkM7K7Umxw6Pwx4xKD2I6R3jQRDfWAQ2YDnfKndC1xJkZo5DWb4D7iF-jX9Eqz_fuD3L2gudKPC_IbcgKsYgjLknAJxJzicjV_0DWGa_E6RdEKQbAz72XwACETzAAAAAF80cCNAA"
+FORCESUB = "hackermittron"
+AUTH = 6389088397
 SUDO_USERS = []
 
 if len(AUTH) != 0:
