ba44b98a9191839fadf002a17ee7b4e5cbfe3f54
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-19 15:15:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config:
     API_ID = 26765615
     API_HASH = "dc9335059a80b3c8753d26e36d9e28f4"
-    BOT_TOKEN = "6992559223:AAEMce2gy8ALQPi7pLe8I6PLfjbuOTWBlHc"
+    BOT_TOKEN = "7012933656:AAG5uHDGRQHwH4WhUPi1LKSmQXHt_JDnsFQ"
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = "mongodb+srv://mswpresents01:O1mAmjU4SHgEergF@cluster0.swcwpvm.mongodb.net/?retryWrites=true&w=majority"
     DATABASE_NAME = environ.get("DATABASE_NAME", "hacker")
